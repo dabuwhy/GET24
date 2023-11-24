eb932f552b54df84d6a6a48c0b32352084b7ff20
dabuwhy <dabuwhy@users.noreply.github.com>
2023-11-24 20:24:13 +0800

    fix bug：click and touch diff from OS

--- number.gd
+++ number.gd
@@ -17,7 +17,7 @@ var operators=[]
 var otherRect=null
 var dir=Vector2.ZERO
 var inScreenPos=Vector2.ZERO
-var touchIndex=0
+var touchIndex=-1
 func _init():
 	pass
 
@@ -28,81 +28,99 @@ func _ready():
 	operators.append($operator2)
 	operators.append($operator3)
 	operators.append($operator4)
-	
-func _unhandled_input(event):
-	if isMoving:
-		if (event is InputEventMouseButton &&!event.pressed && event.button_index==MOUSE_BUTTON_LEFT) \
-			|| (event is InputEventScreenTouch && !event.pressed && event.index==touchIndex):
-			if beAdd:
-				var res=Globals.Calc2Num(Globals.rectNumber[Globals.beSelectRect],Globals.rectNumber[self],Globals.operatorIndex)
-				if res>=0:
-					self.collision_layer=2
-					number.collision_layer=2
-					Globals.rectNumber[Globals.beSelectRect]=res
-					Globals.beSelectRect.num=str(Globals.rectNumber[Globals.beSelectRect])
-					while Globals.history.size()>Globals.historyIndex+1:
+func releasedOrBeIn():
+	if beAdd:
+		var res=Globals.Calc2Num(Globals.rectNumber[Globals.beSelectRect],Globals.rectNumber[self],Globals.operatorIndex)
+		if res>=0:
+			self.collision_layer=2
+			number.collision_layer=2
+			Globals.rectNumber[Globals.beSelectRect]=res
+			Globals.beSelectRect.num=str(Globals.rectNumber[Globals.beSelectRect])
+			while Globals.history.size()>Globals.historyIndex+1:
 #						print("popback:",Globals.history)
-						Globals.history.pop_back()
-					var now={}
-					for r in Globals.rectNumber.keys():
-						if r!=self:
-							now[r.name]=[r.num,r.position,r.collision_layer]
-					now[self.name]=[self.num,Globals.beSelectRect.position,self.collision_layer]
-					Globals.history.push_back(now)
-					Globals.historyIndex+=1
+				Globals.history.pop_back()
+			var now={}
+			for r in Globals.rectNumber.keys():
+				if r!=self:
+					now[r.name]=[r.num,r.position,r.collision_layer]
+			now[self.name]=[self.num,Globals.beSelectRect.position,self.collision_layer]
+			Globals.history.push_back(now)
+			Globals.historyIndex+=1
 #					print(Globals.history)
-					emit_signal("MergeNumber")
-					Globals.moveToHide(self,self.position,Globals.beSelectRect.position)
+			emit_signal("MergeNumber")
+			Globals.moveToHide(self,self.position,Globals.beSelectRect.position)
 
-			for o in operators:
-				o.modulate=Color(1,1,1,0.6)
-			isMoving=false
-			number.z_index=4
-#			print(isMoving)
-			number.monitorable=false
-			self.monitorable=true
-		elif event is InputEventMouseMotion || (event is InputEventScreenDrag && event.index==touchIndex):	
-			self.position=event.position-relPos
-	else:
-		if event is InputEventMouseMotion || (event is InputEventScreenDrag && event.index==touchIndex):
-			if beIn && beSelect:
-				if otherRect.global_position.x<self.position.x:
-					Globals.operatorIndex=0
-				else:
-					Globals.operatorIndex=1
-				if otherRect.global_position.y<self.position.y:
-					pass
-				else:
-					Globals.operatorIndex+=2
-				for o in operators:
-					o.modulate=Color(1,1,1,0.6)
-				operators[Globals.operatorIndex].modulate=Color(0.5,0.5,0.5,1)
-			else:
-				for o in operators:
-					o.modulate=Color(1,1,1,0.6)
-
-func _on_input_event(viewport, event, shape_idx):
-	if (event is InputEventMouseButton && event.pressed && event.button_index==MOUSE_BUTTON_LEFT) \
-		|| (event is InputEventScreenTouch && event.pressed):
-#		print(event)
-#		print(self.position,self.global_position)
-		self.monitorable=false
-		self.monitoring=false
-		isMoving=true
+	for o in operators:
+		o.modulate=Color(1,1,1,0.6)
+	self.isMoving=false
+	self.touchIndex=-1
+	number.z_index=4
+#			print(self.isMoving)
+	number.monitorable=false
+	self.monitorable=true
+func MovingNum():
+	if beIn && beSelect:
+		if otherRect.global_position.x<self.position.x:
+			Globals.operatorIndex=0
+		else:
+			Globals.operatorIndex=1
+		if otherRect.global_position.y<self.position.y:
+			pass
+		else:
+			Globals.operatorIndex+=2
 		for o in operators:
-			o.modulate=Color(1,1,1,0)
-#		print(isMoving)
-		number.monitorable=true
-		number.z_index=3
-		relPos=event.position-self.position
-		if event is InputEventScreenTouch:
-			touchIndex=event.index
-	pass # Replace with function body.
+			o.modulate=Color(1,1,1,0.6)
+		operators[Globals.operatorIndex].modulate=Color(0.5,0.5,0.5,1)
+	else:
+		for o in operators:
+			o.modulate=Color(1,1,1,0.6)
+func _unhandled_input(event):
+	if OS.get_name()=="iOS" || OS.get_name()=="Android":
+		if self.isMoving:
+			if event is InputEventScreenTouch && !event.pressed && event.index==self.touchIndex:
+				releasedOrBeIn()
+			elif event is InputEventScreenDrag && event.index==self.touchIndex:	
+				self.position=event.position-relPos
+		else:
+			if event is InputEventScreenDrag:
+				MovingNum()
+	else:
+		if self.isMoving:
+			if event is InputEventMouseButton &&!event.pressed && event.button_index==MOUSE_BUTTON_LEFT:
+				releasedOrBeIn()
+			elif event is InputEventMouseMotion:	
+				self.position=event.position-relPos
+		else:
+			if event is InputEventMouseMotion:
+				MovingNum()
+			
+func chosen(event):
+	self.monitorable=false
+	self.monitoring=false
+	self.isMoving=true
+	for o in operators:
+		o.modulate=Color(1,1,1,0)
+#		print(self.isMoving)
+	number.monitorable=true
+	number.z_index=3
+	relPos=event.position-self.position
+	
+func _on_input_event(viewport, event, shape_idx):
+	if OS.get_name()=="iOS" || OS.get_name()=="Android":
+		if event is InputEventScreenTouch && event.pressed:
+			#print(event)
+			#print(self.position,self.global_position)
+			chosen(event)
+			print(event.position,self.position)
+			self.touchIndex=event.index
+	else:
+		if event is InputEventMouseButton && event.pressed && event.button_index==MOUSE_BUTTON_LEFT:
+			chosen(event)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	self.label.text=num
-	if !isMoving:
+	if !self.isMoving:
 		inScreenPos=self.position.clamp(Vector2.ZERO,get_viewport_rect().size-$CollisionShape2D.shape.size)
 		dir=Vector2.ZERO
 		if inScreenPos.distance_squared_to(self.position) > 4.0:
